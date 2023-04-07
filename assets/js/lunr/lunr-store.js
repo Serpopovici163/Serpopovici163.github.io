9001ed56fbe9ca0139599d5d836a97db1054a2e5
Serban Popovici <serpopovici163@gmail.com>
2023-04-07 12:52:10 -0400

    initial commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,103 @@
+var store = [{
+        "title": "DIY 3D Printed Powered Ascender",
+        "excerpt":"Shouldn’t work but it might I figured that, considering people have 3D printed functional guns, surely it must be possible to 3D print an ascender without worrying about the material’s strength. Initial designs are solely for prototyping and, as such, have no accommodations for the ESCs, batteries, or any hardware...","categories": [],
+        "tags": [],
+        "url": "/powered-ascender/",
+        "teaser": null
+      },{
+        "title": "Annota Application",
+        "excerpt":"Annota is my first ever attempt at developing software for Android devices. As part of a second year course, we were challenged to develop an application that could digitize a user’s notes in order to categorize and index them. This application is far from exemplar for a variety of reasons...","categories": [],
+        "tags": [],
+        "url": "/annota-mobile-application-DESKTOP-Q9T83P9/",
+        "teaser": null
+      },{
+        "title": "Mining Rig Temperature Control",
+        "excerpt":"A solution for outdoor mining rigs during winter I briefly decided to invest in a few graphics cards with all the recent hype surrounding cryptocurrencies and mining. I figured, with GPU prices today, it would be rather impossible to lose money from such an investment and I wanted to try...","categories": [],
+        "tags": [],
+        "url": "/mining-rig-temp-control/",
+        "teaser": null
+      },{
+        "title": "Jaguar (Medium-sized VTOL UAV Platform)",
+        "excerpt":"Simple but limited UAV platform for urban operations I kept seeing ~2.5m wide VTOL UAVs on AliExpress that I would love to own but they are all rather overpriced so I decided to make my own. Using the tech I played with while developing the LTE-based Mavic Pro I intend...","categories": [],
+        "tags": [],
+        "url": "/jaguar/",
+        "teaser": null
+      },{
+        "title": "Roof Anchor Drone",
+        "excerpt":"A physical infiltration tool This device was originally designed in hopes of rooftopping the second tallest building in Canada at the time of this writing: The St. Regis hotel in Toronto. The concern was that the lower roof at the St. Regis hotel is accessible while the higher roof is...","categories": [],
+        "tags": [],
+        "url": "/roof-anchor-drone/",
+        "teaser": null
+      },{
+        "title": "Smart Holographic Sight",
+        "excerpt":"Intelligent weapon optics My overarching goal with a lot of the things I build is to develop a small ecosystem of tactically useful devices that can exchange information and provide the user with an advantage over their adversary. These devices include anything from motorized ascenders to augmented reality devices and...","categories": [],
+        "tags": [],
+        "url": "/smart-sight/",
+        "teaser": null
+      },{
+        "title": "Security Camera Privacy Hack",
+        "excerpt":"Enhancing privacy of cheap security cameras get picture of whole camera to put here as well I’ve always been a little paranoid of having IoT devices live streaming views of my living space however I have a long trip coming up so a couple security cameras would be nice to...","categories": [],
+        "tags": [],
+        "url": "/camera-privacy-hack/",
+        "teaser": null
+      },{
+        "title": "LTE Mavic Pro",
+        "excerpt":"Cellular-based drone control has been a longstanding goal with the purpose of creating UAVs requiring minimal user input to function. I have found the need for constant input to significantly reduce the useability of drones for recon during my adventures because neither myself nor the people with me can dedicate...","categories": [],
+        "tags": [],
+        "url": "/lte-mavic-pro/",
+        "teaser": null
+      },{
+        "title": "BRZ CAN",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/brz-CAN/",
+        "teaser": null
+      },{
+        "title": "BRZ VIM4",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/brz-VIM4/",
+        "teaser": null
+      },{
+        "title": "BRZ Head Unit",
+        "excerpt":"This page outlines the process of modifying the BRZ’s stock head unit to contain custom components. I maintained the casing of the original head unit to avoid designing custom mounting hardware and alleviate the need to reverse engineer the casing design. Architecture include block diagram of design here CAD The...","categories": [],
+        "tags": [],
+        "url": "/brz-head-unit/",
+        "teaser": null
+      },{
+        "title": "BRZ LightLink",
+        "excerpt":"LightLink Overview Control of a vehicle’s external lighting is safety critical, so I’ve settled on designing a fully-redundant custom circuit board with 12 MOSFET-driven channels to handle ‘dumb’ lights and 8 addressable channels to handle a set of addressable LED arrays retrofitted throughout the vehicle. In addition to designing a...","categories": [],
+        "tags": [],
+        "url": "/brz-light-link/",
+        "teaser": null
+      },{
+        "title": "BRZ Steering Wheel",
+        "excerpt":"Steering Wheel Button Integration Steering wheel buttons were added to BRZs in 2017 and mine is a 2013 so I purchased a salvage steering wheel in order to integrate its buttons into my systems. Should anybody else contemplate to do the same, be aware that even though steering wheels are...","categories": [],
+        "tags": [],
+        "url": "/brz-steering-wheel/",
+        "teaser": null
+      },{
+        "title": "BRZ Wiring",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/brz-wiring/",
+        "teaser": null
+      },{
+        "title": "Modernizing my 2013 Subaru BRZ",
+        "excerpt":"I recently acquired a car for recreational purposes since I had saved up a decent amount of money and didn’t feel like investing it. I purchased a BRZ because they are built in conjunction with Toyota and this should theoretically imply that they are relatively reliable and I won’t need...","categories": [],
+        "tags": [],
+        "url": "/brz/",
+        "teaser": null
+      },{
+        "title": "LoRa Sensor Suite",
+        "excerpt":"A scalable dynamic solution for monitoring activity in unknown environments At some point in 2021, a friend and I had just finished exploring a construction site and as we were preparing to leave on the first floor, a security guard haphazardly walked in. Luckily we were out of sight and...","categories": [],
+        "tags": [],
+        "url": "/lora-sensor-suite/",
+        "teaser": null
+      },{
+        "title": "Two G-class Stage Rocket w/ Basic Telemetry",
+        "excerpt":"Experimenting with rocketry electronics While I doubt I will ever get a high power rocketry license, I do thoroughly enjoy developing and flying model rockets. None of my past rockets have incorporated any amount of electronics so the purpose of this project is to get a feel for rocketry electronics...","categories": [],
+        "tags": [],
+        "url": "/two-stage-rocket/",
+        "teaser": null
+      }]
