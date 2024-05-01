3fc1fc521778e926029c64d8a74c6f43a3042c45
Serban Popovici <serpopovici163@gmail.com>
2024-05-01 09:04:26 -0400

    please work

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "DIY 3D Printed Powered Ascender",
+        "excerpt":"Shouldn’t work but it might I figured that, considering people have 3D printed functional guns, surely it must be possible to 3D print an ascender without worrying about the material’s strength. Initial designs are solely for prototyping and, as such, have no accommodations for the ESCs, batteries, or any hardware...","categories": [],
+        "tags": [],
+        "url": "/powered-ascender/",
+        "teaser": null
+      },{
+        "title": "Annota Application",
+        "excerpt":"Annota is my first ever attempt at developing software for Android devices. As part of a second year course, we were challenged to develop an application that could digitize a user’s notes in order to categorize and index them. This application is far from exemplar for a variety of reasons...","categories": [],
+        "tags": [],
+        "url": "/annota-mobile-application-DESKTOP-Q9T83P9/",
+        "teaser": null
+      },{
+        "title": "Mining Rig Temperature Control",
+        "excerpt":"A solution for outdoor mining rigs during winter I briefly decided to invest in a few graphics cards with all the recent hype surrounding cryptocurrencies and mining. I figured, with GPU prices today, it would be rather impossible to lose money from such an investment and I wanted to try...","categories": [],
+        "tags": [],
+        "url": "/mining-rig-temp-control/",
+        "teaser": null
+      },{
+        "title": "Jaguar (Medium-sized VTOL UAV Platform)",
+        "excerpt":"Simple but limited UAV platform for urban operations I kept seeing ~2.5m wide VTOL UAVs on AliExpress that I would love to own but they are all rather overpriced so I decided to make my own. Using the tech I played with while developing the LTE-based Mavic Pro I intend...","categories": [],
+        "tags": [],
+        "url": "/jaguar/",
+        "teaser": null
+      },{
+        "title": "Roof Anchor Drone",
+        "excerpt":"A physical infiltration tool This device was originally designed in hopes of rooftopping the second tallest building in Canada at the time of this writing: The St. Regis hotel in Toronto. The concern was that the lower roof at the St. Regis hotel is accessible while the higher roof is...","categories": [],
+        "tags": [],
+        "url": "/roof-anchor-drone/",
+        "teaser": null
+      },{
+        "title": "Smart Holographic Sight",
+        "excerpt":"Intelligent weapon optics My overarching goal with a lot of the things I build is to develop a small ecosystem of tactically useful devices that can exchange information and provide the user with an advantage over their adversary. These devices include anything from motorized ascenders to augmented reality devices and...","categories": [],
+        "tags": [],
+        "url": "/smart-sight/",
+        "teaser": null
+      },{
+        "title": "LoRa Sensor Suite",
+        "excerpt":"A scalable dynamic solution for monitoring activity in unknown environments At some point in 2021, a friend and I had just finished exploring a construction site and as we were preparing to leave on the first floor, a security guard haphazardly walked in. Luckily we were out of sight and...","categories": [],
+        "tags": [],
+        "url": "/lora-sensor-suite/",
+        "teaser": null
+      },{
+        "title": "LTE Mavic Pro",
+        "excerpt":"Cellular-based drone control has been a longstanding goal with the purpose of creating UAVs requiring minimal user input to function. I have found the need for constant input to significantly reduce the useability of drones for recon during my adventures because neither myself nor the people with me can dedicate...","categories": [],
+        "tags": [],
+        "url": "/lte-mavic-pro/",
+        "teaser": null
+      },{
+        "title": "Creating a Watch Dogs-type app",
+        "excerpt":"The premise of Ubisoft’s Watch Dogs video game series has always fascinated me. Some of the exploits you can pull off with your in-game cell phone are far fetched but most are genuinely possible with the right amount of software work. I’ve personally always been a fan of rooftopping, moreso...","categories": [],
+        "tags": [],
+        "url": "/alexnet/",
+        "teaser": null
+      },{
+        "title": "BRZ CAN",
+        "excerpt":"This page is mostly for my personal reference but will also showcase some of the hardware used to wire my CAN hardware  ","categories": [],
+        "tags": [],
+        "url": "/brz-CAN/",
+        "teaser": null
+      },{
+        "title": "BRZ Custom Hardware",
+        "excerpt":"This page contains any custom PCBs I’ve ordered for the car. The two PCBs currently included are the LightLink module and a custom wake-on-CAN switch, used to cut off power delivery to computers within the car when they are not needed. UPDATE July 2023 Boards ordered :) I really hope...","categories": [],
+        "tags": [],
+        "url": "/brz-custom-hardware/",
+        "teaser": null
+      },{
+        "title": "BRZ Gallery",
+        "excerpt":"Accident I managed to rear end someone in this car, definitely my fault, but I was not willing to let it die that soon because I had barely done any work on it. The accident luckily wasn’t very bad, the other car suffered next to no damage and mine took...","categories": [],
+        "tags": [],
+        "url": "/brz-gallery/",
+        "teaser": null
+      },{
+        "title": "BRZ Head Unit",
+        "excerpt":"This page outlines the process of modifying the BRZ’s stock head unit to contain custom components. I maintained the casing of the original head unit to avoid designing custom mounting hardware and alleviate the need to reverse engineer the casing/mounting point dimensions. Architecture include block diagram of design here Final...","categories": [],
+        "tags": [],
+        "url": "/brz-head-unit/",
+        "teaser": null
+      },{
+        "title": "BRZ Lighting",
+        "excerpt":"Light Modifications The car uses my custom LightLink module will be installed under the steering column, next to the car’s body control module which provides it with immediate access to all of the car’s stock lighting wiring. I may have to run some extra wires for things like my brake...","categories": [],
+        "tags": [],
+        "url": "/brz-light-link/",
+        "teaser": null
+      },{
+        "title": "BRZ Steering Wheel",
+        "excerpt":"Steering Wheel Button Integration Steering wheel buttons were added to BRZs in 2017 and mine is a 2013 so I purchased a salvage steering wheel in order to integrate its buttons into my systems. Should anybody else contemplate to do the same, be aware that even though steering wheels are...","categories": [],
+        "tags": [],
+        "url": "/brz-steering-wheel/",
+        "teaser": null
+      },{
+        "title": "BRZ User Interface",
+        "excerpt":"Head Unit Display The large head unit display is split horizontally into two sections: a ‘big container’ which is 3/4 of the display’s width and a ‘small container’ which occupies the remaining space. The views displayed by the big container and their respective purpose are as follows: Navigation fragment –&gt;...","categories": [],
+        "tags": [],
+        "url": "/brz-user-interface/",
+        "teaser": null
+      },{
+        "title": "BRZ Vision System",
+        "excerpt":"General Overview All cameras are wired to the center console, near the airbag module of the car, where I will mount a nVidia Jetson Orin NX. Any cameras mounted somewhere that could be reasonably reached by water have been doused in 422C conformal coating by MG Chemicals to provide an...","categories": [],
+        "tags": [],
+        "url": "/brz-vision-system/",
+        "teaser": null
+      },{
+        "title": "BRZ Wiring",
+        "excerpt":"Don’t even get me started. General overview This page is somewhat just documentation for myself with regards to connector pinouts but also contains some general information lower down. I bought a bunch of ‘waterproof’ connectors off of AliExpress for this project. I say waterproof in quotes because I disconnected one...","categories": [],
+        "tags": [],
+        "url": "/brz-wiring/",
+        "teaser": null
+      },{
+        "title": "Modernizing my 2013 Subaru BRZ",
+        "excerpt":"UPDATE This page is very much under construction, with the nice weather finally here I have begun work on the car and certain things will change/be populated as I get to them. I apologize for the organizational mess that is this page. GitHub: https://github.com/Serpopovici163/BRZ I recently acquired a car for...","categories": [],
+        "tags": [],
+        "url": "/brz/",
+        "teaser": null
+      },{
+        "title": "Security Camera Privacy Hack",
+        "excerpt":"Enhancing privacy of cheap security cameras I’ve always been a little paranoid of having IoT devices live streaming views of my living space however I have a long trip coming up so a couple security cameras would be nice to keep an eye on my place. I considered a couple...","categories": [],
+        "tags": [],
+        "url": "/camera-privacy-hack/",
+        "teaser": null
+      },{
+        "title": "Two G-class Stage Rocket w/ Basic Telemetry",
+        "excerpt":"Experimenting with rocketry electronics While I may never get a high power rocketry license, I do thoroughly enjoy developing and flying model rockets. None of my past rockets have incorporated any amount of electronics so the purpose of this project is to get a feel for rocketry electronics without any...","categories": [],
+        "tags": [],
+        "url": "/two-stage-rocket/",
+        "teaser": null
+      },{
+        "title": "HYDRA: Modular Avionics",
+        "excerpt":"This page briefly summarizes the extensive amount of work that went into designing the avionics system for the University of Ottawa’s rocketry team. We unfortunately had to scrap last year’s work since it was very unorganized and we had absolutely no documentation to work off of. For this project, I...","categories": [],
+        "tags": [],
+        "url": "/hydra/",
+        "teaser": null
+      }]
